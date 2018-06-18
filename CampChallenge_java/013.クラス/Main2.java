22ff8b89d591b40c5c36a6452737abde19fc3a2c
EXP-0017\guest1Day <tecni.tourna.gs0522@gmail.com>
2018-06-18 17:14:01 +0900

    クラスの継承

---
+++ CampChallenge_java/013.クラス/Main2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Main2 {
+    public static void main(String[] args){
+     Teacher sensei=new Teacher();
+     sensei.reset();
+    }
+    
+}
