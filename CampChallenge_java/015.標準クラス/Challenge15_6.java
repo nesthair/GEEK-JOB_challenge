c1c39e3b6cd1bd9d2bde696cd7ced01593375386
EXP-0017\guest1Day <tecni.tourna.gs0522@gmail.com>
2018-06-28 14:14:21 +0900

    文字数の取得

---
+++ CampChallenge_java/015.標準クラス/Challenge15_6.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Challenge15_6 {
+    public static void main(String[] args){
+        String address="tecni.tourna.gs0522@gmail.com";
+        System.out.println(address.substring(19).length());
+    }
+    
+    
+    
+}
