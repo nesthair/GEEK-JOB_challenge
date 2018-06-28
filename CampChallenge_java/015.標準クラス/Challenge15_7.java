c69cd49d652a16a4b8ea771f1aaecb03761544be
EXP-0017\guest1Day <tecni.tourna.gs0522@gmail.com>
2018-06-28 15:29:47 +0900

    文字の入れ替え

---
+++ CampChallenge_java/015.標準クラス/Challenge15_7.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Challenge15_7 {
+    public static void main(String[] args){
+        String word="きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        word=word.replace("I","い");
+        word=word.replace("U","う");
+        System.out.println(word);
+    }
+    
+}
