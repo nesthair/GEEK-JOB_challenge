49c1b4c85ef3c5dfd619cd0a9d1839116db9edae
EXP-0017\guest1Day <tecni.tourna.gs0522@gmail.com>
2018-07-23 14:24:52 +0900

    ファイルの書き出しと保存

---
+++ CampChallenge_java/015.標準クラス/SelfIntroduce.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author guest1Day
+ */
+public class SelfIntroduce {
+    public static void main(String[] args){
+        System.out.println("私の名前は安西です。");
+        System.out.println("二十歳です。");
+    }
+}
